fae2ffaee11667d00427e71fa24e2da391666432
Haripada <jenaharipada@gmail.com>
2024-10-15 20:45:38 +0530

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,22 @@ use App\Http\Controllers\FirstController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome'); //starting page
+    return view('welcome'); 
 });
 
-Route::get('myname',function(){
-    return view('test');
+
+Route::get('/index',function(){
+    return view('index');
 });
+
 Route::get('form1',function(){
     return view(view: 'form.form1');
 });
 Route::get('form2',function(){
     return view(view: 'form.form2');
 });
+
 Route::view('/form3','welcome');
 
-Route::get('/website/form1',[FirstController::class,'Form1']);
-Route::redirect('/form3','/',200);
+//Route::get('/website/form1',[FirstController::class,'Form1']);
+// Route::redirect('/form3','/',200);
